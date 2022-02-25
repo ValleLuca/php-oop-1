ab09174d13eca2081d104a395d1475a956240432
ValleLuca <valle.lucaAb@outlook.it>
2022-02-25 16:26:11 +0100

    Conclusione lavoro su OOP

---
+++ classes/Movie.php
@@ -0,0 +1,24 @@
+<?php
+class Movie {
+    private $nome;
+    private $cognome;
+    private $data = 2022;
+    private $classe;
+
+    public function __construct($_name, $_cognome)
+    {
+        $this->nome = $_name;
+        $this->cognome = $_cognome;
+        $this->classe = 51;
+
+    }
+    public function getNomeCognome() {
+        return "{$this->nome} {$this->cognome}";
+    }
+    public function getData() {
+        return "{$this->data}";
+    }
+    public function getClasse() {
+        return "{$this->classe}";
+    }
+}
---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+include __DIR__.'/classes/Movie.php';
+
+$utente = new Movie("Luca", "Valle");
+
+$NomeCognome = $utente->getNomeCognome();
+$Data = $utente->getData();
+$Classe = $utente->getClasse();
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Risultato Stampato</title>
+</head>
+<body>
+    
+    <div>
+        <h2><?php echo $NomeCognome; ?></h2>
+        <p>Nell'anno: <?php echo $Data; ?>, nella classe <?php echo $Classe; ?></p>
+        
+    </div>
+
+</body>
+</html>
